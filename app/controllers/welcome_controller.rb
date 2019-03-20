b40de6981095e6cc2db4dd5846d87f0f41144d1f
SebaCardenas <sacardenas@uc.cl>
2019-03-20 19:08:39 -0300

    client

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+   def index
+    url = 'https://swapi.co/api/films'
+    response = RestClient.get(url)
+    json = JSON.parse(response)
+    @object = json["results"]
+   end
 end
