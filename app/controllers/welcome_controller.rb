08f9f69613457c4f2a85de6fae0bc1fdfa806660
SebaCardenas <sacardenas@uc.cl>
2019-03-21 00:13:02 -0300

    segunda página sin botones

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,21 @@
 class WelcomeController < ApplicationController
-   def index
-    url = 'https://swapi.co/api/films'
-    response = RestClient.get(url)
-    json = JSON.parse(response)
-    @object = json["results"]
-   end
-   def show
-    render template: "welcome/especificacion"
-  end
+    def index
+        url = 'https://swapi.co/api/films'
+        response = RestClient.get(url)
+        json = JSON.parse(response)
+        @object = json["results"]
+        end
+      def show
+    
+        if params[:p_id]
+          @pp_id = params[:p_id]
+          url2 = @pp_id 
+          response2 = RestClient.get(url2)
+          json2 = JSON.parse(response2)
+          @new_item = json2
+        end
+        render template: "welcome/especificacion"
+    
+      end
   
 end
