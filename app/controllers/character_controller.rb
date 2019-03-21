0d626715df8a9baf3f01411ae9e726a36e4b8c15
SebaCardenas <sacardenas@uc.cl>
2019-03-21 14:43:36 -0300

    characters

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,2 +1,11 @@
 class CharacterController < ApplicationController
+    def index
+        if params[:pl_id]
+            @pp_id = params[:pl_id]
+            url2 = @pp_id 
+            response2 = RestClient.get(url2)
+            json2 = JSON.parse(response2)
+            @new_item2 = json2
+        end
+    end
 end
