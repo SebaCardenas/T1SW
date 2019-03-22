45305ad88e9ffb1811e3cca152c186642e0beb0e
SebaCardenas <sacardenas@uc.cl>
2019-03-21 21:29:05 -0300

    movie link2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,19 +6,7 @@ class WelcomeController < ApplicationController
     json = JSON.parse(response)
     @object = json["results"]
     end
-  def show
 
-    if params[:p_id]
-      @pp_id = params[:p_id]
-      url2 = @pp_id 
-      response2 = RestClient.get(url2)
-      json2 = JSON.parse(response2)
-      @new_item = json2
-    end
-    render template: "welcome/especificacion"
-
-
-  end
 
   
 
