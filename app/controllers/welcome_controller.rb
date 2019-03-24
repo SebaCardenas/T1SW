cd0118678081ed60056968ecf0f5f53571bd4e41
SebaCardenas <sacardenas@uc.cl>
2019-03-24 18:24:38 -0300

    search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,43 @@ class WelcomeController < ApplicationController
     @object = json["results"]
     end
 
+  def search  
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+      @parameter = params[:search].downcase  
+      url = 'https://swapi.co/api/'
+      response = RestClient.get(url)
+      json2 = JSON.parse(response)
+      @object2 = json2
 
+      @object2.each do |tipo|
+        url = tipo[1]
+        response = RestClient.get(url)
+        json3 = JSON.parse(response)
+        @object3 = json3["results"]
+        @object3.each do |nombre|
+        if url == 'https://swapi.co/api/films/'
+          if nombre['title'].downcase == @parameter
+            redirect_to pelicula_index_path(:p_id => nombre['url'])
+          end
+        elsif url == 'https://swapi.co/api/people/'
+          if nombre['name'].downcase == @parameter
+            redirect_to character_index_path(:pl_id => nombre['url'])
+          end
+        elsif url == 'https://swapi.co/api/planets/'
+          if nombre['name'].downcase == @parameter
+            redirect_to planet_index_path(:pl_id => nombre['url'])
+          end
+        elsif url == 'https://swapi.co/api/starships/'
+          if nombre['name'].downcase == @parameter
+            redirect_to nave_index_path(:pl_id => nombre['url'])
+          end
+        end
+        end 
+      end
+    end  
+  end
   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   root 'planet#index'
   root 'character#index'
   root 'nave#index'
+
+  get '/search' => 'welcome#search', :as => 'search_welcome'
+  get '/search' => 'pelicula#search', :as => 'search_pelicula'
+  get '/search' => 'planet#search', :as => 'search_planet'
+  get '/search' => 'character#search', :as => 'search_character'
+  get '/search' => 'nave#search', :as => 'search_nave'
+
   
 end
