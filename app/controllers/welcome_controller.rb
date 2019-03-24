c5ca3a7d06450bfa2fb1d07cdde326ba58682206
SebaCardenas <sacardenas@uc.cl>
2019-03-24 20:45:02 -0300

    searchlist

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -48,7 +48,7 @@ class WelcomeController < ApplicationController
             end 
         end
         if @verificador == false
-            redirect_to search_welcome_path, :alert => 'NOT FOUND!
+            redirect_to(search_welcome_path, :alert => 'NOT FOUND!') and return
         end 
     end
   end
