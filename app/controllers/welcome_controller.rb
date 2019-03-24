11e4bbf3c1140867855b8cd8f6b8cda4f2be7e81
SebaCardenas <sacardenas@uc.cl>
2019-03-24 19:46:39 -0300

    searchlist

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -48,7 +48,7 @@ class WelcomeController < ApplicationController
             end 
         end
         if @verificador == false
-            redirect_to rooth_path
+            redirect_to root_path
         end 
     end
   end
